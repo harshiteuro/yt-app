6db2b15339eb28660cdeb0d8edae5369dbe5b764
harshit <harshiteuro@gmail.com>
2022-10-26 19:48:50 +0530

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+
+const store=createStore(reducers);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App></App>
+    </Provider>
+ 
+);
